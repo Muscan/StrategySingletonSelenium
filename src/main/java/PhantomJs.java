3d59bd0e41bc483d8bc417d69e98b4db69902c3e
Mihai Muscan <mihai.muscan@seltronix.eu>
2022-08-03 15:05:28 +0300

    Selenium Strategy, Singleton on a public website

---
+++ src/main/java/PhantomJs.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class PhantomJs implements  DriverStrategy{
+    //phantom  js is a headless browser
+    //good memory performance
+    //not supported on all browser
+    @Override
+    public WebDriver setStrategy(){
+        System.setProperty("phantomjs.binary.path","src/main/resources/phantom.exe");
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        //make sure js is enabled on browsers for the tests
+        desiredCapabilities.setJavascriptEnabled(true);
+        WebDriver driver = new PhantomJSDriver(desiredCapabilities);
+        return  driver;
+    }
+}
