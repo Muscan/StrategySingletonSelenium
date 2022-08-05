57a01c076533cc1942cba3633f055f62fd367333
Mihai <kubikweg@gmail.com>
2022-08-05 17:11:32 +0300

    Added pages, Constants, Utils using decode for pw and mail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 import drivers.DriverSingleton;
 import org.openqa.selenium.WebDriver;
+import pages.CheckoutPage;
+import pages.HomePage;
+import pages.SignInPage;
+import utils.Constants;
 import utils.FrameworkProperties;
 
 public class Main {
@@ -8,7 +12,21 @@ public class Main {
         DriverSingleton.getInstance(frameworkProperties.getProperty("browser"));
         WebDriver driver = DriverSingleton.getDriver();
         driver.get("http://automationpractice.com");
-        DriverSingleton.closeObjectInstance();
+        HomePage homePage = new HomePage();
+        homePage.addFirstElementToCart();
+        homePage.addSecondElementToCart();
+
+        CheckoutPage checkoutPage = new CheckoutPage();
+        checkoutPage.goToCheckout();
+
+        SignInPage signInPage = new SignInPage();
+        signInPage.logIn(frameworkProperties.getProperty(Constants.EMAIL), frameworkProperties.getProperty(Constants.PASSWORD));
+
+        checkoutPage.confirmAddress();
+        checkoutPage.confirmShipping();
+        checkoutPage.payByBankWire();
+        checkoutPage.confirmFinalOrder();
+        if(checkoutPage.checkFinalStatus())
+            System.out.println("Test case completed!");
     }
 }
-
